f316a9c7f3d405a16d1ce98a6206d5847281a634
Lora Whitehead <lola50wise@gmail.com>
2020-11-28 13:36:18 -0800

    This is genXer code I finally got compiled because I updated to use the correct typescript version

---
+++ src/app/gen-xer/gen-xer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gen-xer',
+  templateUrl: './gen-xer.component.html',
+  styleUrls: ['./gen-xer.component.css']
+})
+export class GenXerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
